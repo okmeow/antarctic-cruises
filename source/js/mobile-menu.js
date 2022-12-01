3316e2fc9e1f2d8bfe147f907a1423a4b05763b6
Ilia <ilytar@gmail.com>
2022-12-01 19:30:09 +0300

    Мобильное меню закрывается при клике вне его

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,14 +1,22 @@
 const nav = document.querySelector('.navigation');
 const navToggle = document.querySelector('.navigation__toggle');
 const body = document.querySelector('[data-body="body"]');
+const navLinks = document.querySelectorAll('[ data-link="nav-link"]');
 
 nav.classList.remove('navigation--nojs');
+
+navLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    body.style.overflow = 'scroll';
+  });
+});
 
 const openMobileMenu = () => {
   nav.classList.remove('navigation--closed');
   navToggle.classList.remove('navigation__toggle--open');
   navToggle.classList.add('navigation__toggle--close');
   body.style.overflow = 'hidden';
+  document.addEventListener('mouseup', clickOutsideModalMenu);
 };
 
 const closeMobileMenu = () => {
@@ -16,6 +24,7 @@ const closeMobileMenu = () => {
   navToggle.classList.remove('navigation__toggle--close');
   navToggle.classList.add('navigation__toggle--open');
   body.style.overflow = 'scroll';
+  document.removeEventListener('mouseup', clickOutsideModalMenu);
 };
 
 const toggleMobileMenu = () => {
@@ -28,6 +37,13 @@ const toggleMobileMenu = () => {
 
 const mobileMenuHandler = () => {
   navToggle.addEventListener('click', toggleMobileMenu);
+};
+
+const clickOutsideModalMenu = (evt) => {
+  if (evt.target.classList.contains('navigation__list')) {
+    return evt;
+  }
+  return closeMobileMenu();
 };
 
 export {mobileMenuHandler};
