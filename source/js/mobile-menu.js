5b3ece1ee5f2d480b71447e62686b830d72a93ec
Ilia <ilytar@gmail.com>
2022-12-01 18:39:34 +0300

    Убирает скролл при открытом мобильном меню

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,5 +1,6 @@
-let nav = document.querySelector('.navigation');
-let navToggle = document.querySelector('.navigation__toggle');
+const nav = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toggle');
+const body = document.querySelector('[data-body="body"]');
 
 nav.classList.remove('navigation--nojs');
 
@@ -7,12 +8,14 @@ const openMobileMenu = () => {
   nav.classList.remove('navigation--closed');
   navToggle.classList.remove('navigation__toggle--open');
   navToggle.classList.add('navigation__toggle--close');
+  body.style.overflow = 'hidden';
 };
 
 const closeMobileMenu = () => {
   nav.classList.add('navigation--closed');
   navToggle.classList.remove('navigation__toggle--close');
   navToggle.classList.add('navigation__toggle--open');
+  body.style.overflow = 'scroll';
 };
 
 const toggleMobileMenu = () => {
